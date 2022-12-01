59f58624f90d110778d2b22edf07c043e70c814d
jassus213 <nikitahunter23@gmail.com>
2022-12-01 13:13:22 +0300

    #14 take order def

--- Courier/TypeOfCouriers/Courier.py
+++ Courier/TypeOfCouriers/Courier.py
@@ -15,7 +15,15 @@ class Courier:
 
     def attach_order(self, order):
         print("Курьер с номером: " + str(self.id) + " выполнит заказ номер: " + str(order.id))
+        self.take_order(order)
         self.change_weight(order.weight)
+
+    def take_order(self, order):
+        print(self.x_coord, self.y_coord)
+        self.x_coord = order.x_coord
+        self.y_coord = order.y_coord
+        print(str(self.id) + " Забрал заказ: " + str(order.id))
+        print(self.x_coord, self.y_coord)
 
     def change_weight(self, order_weight):
         self.maxWeight -= order_weight
